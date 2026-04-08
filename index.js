1ef8c7d95faa25d9606d251a9d2d1365e1f2e883
estefanidex <denis.poker10@gmail.com>
2026-04-08 09:47:12 -0300

    Add /api compatibility for public routes

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ app.use((req, res, next) => {
 
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
+app.use('/api', publicRoutes);
 app.use(publicRoutes);
 
 app.get('/favicon.ico', (req, res) => {
